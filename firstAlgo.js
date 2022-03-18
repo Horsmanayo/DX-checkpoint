8846a2695a70cc5105a8ea2798db67581ba8c041
Horsmanayo <oniademola001@gmail.com>
2022-03-18 09:35:44 +0100

    'updated commit'

--- firstAlgo.js
+++ firstAlgo.js
@@ -6,7 +6,9 @@
 //  the highest number of vowels. Ignore any character in the sentence that is
 //  not a letter or a space. Find the most efficient way to achieve this.
 
-// function to get the length of the longest word in string
+// function to get the length of the longest word in string,
+// i am getting the length longest word in the sentence to be able to
+// check through the array of which word matches the length of the longest word
 const getLength = (string) => {
   const num = string
     .replace(/,/g, "")
@@ -17,6 +19,7 @@ const getLength = (string) => {
 };
 
 //filtering words that has the same length as above
+// checking for words that matches the length of the longest words to see if they are more than one
 
 const filterLongestWords = (arr, num) => {
   const longestWords = arr.filter((word) => word.length === num);
@@ -25,6 +28,7 @@ const filterLongestWords = (arr, num) => {
 };
 
 //returning the word with the most vowel in an array
+// looping through the longest if the array return
 const mostVowels = (arr) => {
   let vowels = [..."aeiouAEIOU"];
 
@@ -49,7 +53,7 @@ const mostVowels = (arr) => {
 };
 
 const getLongestWords = (str) => {
-  const arr = string.split(" ");
+  const arr = str.replace(/,/g, "").split(" ");
   const len = getLength(str);
   const newArr = filterLongestWords(arr, len);
   if (newArr.length === 1) {
@@ -62,6 +66,6 @@ const getLongestWords = (str) => {
 
 ///testing algorithim
 let sentence =
-  "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers";
+  "Smart people learn from everything and everyone, average people from their experience, stupid people already, have all the answers, economomically, economicelyeee";
 
 console.log(getLongestWords(sentence));
