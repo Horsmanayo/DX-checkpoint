21b47b59c97260d509876854ea61bdb3cf79efc7
Horsmanayo <oniademola001@gmail.com>
2022-07-14 21:47:53 +0100

    take-home

---
+++ paradigmTakeHome.js
@@ -0,0 +1,33 @@
+//Given an array of integers nums and an integer target,
+//create a function that returns the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution,
+//and you may not use the same element twice.
+
+// Example:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [2,7]
+// Explanation: Because 2 + 7 == 9, we return [2, 7]
+
+function twoSum(nums, target) {
+  //object to keep track of values in the array
+  const previousValues = {};
+  for (let i = 0; i < nums.length; i++) {
+    //saving each value in the array into a variable
+    const currentValue = nums[i];
+    // console.log("currenVal:", currentValue);
+    //getting the first value to compare to other value that sums up to the target
+    const neededValue = target - currentValue;
+    // console.log("needed:", neededValue);
+
+    if (previousValues[neededValue] != null) {
+      //   console.log(previousValues[neededValue]);
+      return [neededValue, currentValue];
+    } else {
+      previousValues[currentValue] = i;
+    }
+  }
+
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
